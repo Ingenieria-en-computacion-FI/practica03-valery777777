cb28dad0963dc6d1e74cd033e760bd771e415ace
valery777777 <valery.padper@gmail.com>
2026-03-08 16:43:25 -0600

    se completó el nodo

--- nodo/main.c
+++ nodo/main.c
@@ -1,30 +1,14 @@
+#include "Nodo.h"
 #include <stdio.h>
-#include "nodo.h"
 
-int main()
-{
-    printf("Prueba del TAD Nodo\n");
-
-    /* TODO
-       1 Crear un nodo con valor 10
-    */
-    Nodo* n = NULL;
-
-    /* TODO
-       2 Mostrar el valor del nodo usando obtenerValor
-    */
-
-    /* TODO
-       3 Cambiar el valor a 25 usando asignarValor
-    */
-
-    /* TODO
-       4 Mostrar nuevamente el valor
-    */
-
-    /* TODO
-       5 Liberar el nodo con destruirNodo
-    */
-
-    return 0;
+int main(){
+    Nodo *n;
+    n = crearNodo(2);
+    printf("El valor del nodo es: %d\n", obtenerValor(n));
+    asignarValor(n, 7);
+    printf("El valor del nodo es: %d\n", obtenerValor(n));
+    destruirNodo(n);
+    n=NULL;
+    (n==NULL)? printf("El nodo es nulo\n") : printf("El nodo no es nulo\n");
+    return 0; 
 }
--- nodo/nodo.h
+++ nodo/nodo.h
@@ -1,20 +1,15 @@
 #ifndef __NODO_H__
 #define __NODO_H__
 
-/* Declaración incompleta del tipo Nodo (tipo opaco) */
-typedef struct Nodo Nodo;
+struct nodo{
+    int valor;
+};
 
+typedef struct nodo Nodo;
 
-/* Crea un nodo con el valor indicado */
-Nodo* crearNodo(int valor);
-
-/* Obtiene el valor almacenado en el nodo */
-int obtenerValor(Nodo* nodo);
-
-/* Cambia el valor almacenado en el nodo */
-void asignarValor(Nodo* nodo, int valor);
-
-/* Libera la memoria del nodo */
-void destruirNodo(Nodo* nodo);
+Nodo *crearNodo(int valor);
+int obtenerValor(const Nodo*);
+void asignarValor(Nodo*, int);
+void destruirNodo(Nodo *);
 
 #endif
